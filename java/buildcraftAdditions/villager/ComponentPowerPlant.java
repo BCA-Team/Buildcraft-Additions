1632ccc4c0f9d2cc7687f5fb9ced8b1933ffbbce
AEnterprise <aenterprise2@gmail.com>
2014-05-14 15:03:01 +0200

    fix powerplant not generating outside of dev environment

--- java/buildcraftAdditions/villager/ComponentPowerPlant.java
+++ java/buildcraftAdditions/villager/ComponentPowerPlant.java
@@ -1,11 +1,15 @@
 package buildcraftAdditions.villager;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
+import java.nio.Buffer;
 import java.util.List;
 import java.util.Random;
 
@@ -16,6 +20,7 @@ import buildcraft.api.blueprints.BlueprintDeployer;
 import buildcraft.core.blueprints.RealBlueprintDeployer;
 import buildcraft.transport.BlockGenericPipe;
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.util.ResourceLocation;
@@ -27,6 +32,7 @@ import net.minecraft.world.gen.structure.StructureVillagePieces.Start;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class ComponentPowerPlant extends StructureVillagePieces.House1 {
+	public static final ResourceLocation blueprint = new ResourceLocation("bcadditions", "blueprints/Redstoneengine-357d6c42def2dad519636d8e7980d72a5284b3de708febd300bb1fc1525f785b.bpt");
 	
 	private int averageGroundLevel = -1;
 	
@@ -60,23 +66,31 @@ public class ComponentPowerPlant extends StructureVillagePieces.House1 {
 
             this.boundingBox.offset(0, this.averageGroundLevel - this.boundingBox.maxY + 11, 0);
         }
-        /*InputStream stream = ComponentPowerPlant.class.getResourceAsStream("/assets/bcadditions/blueprints/Redstoneengine-357d6c42def2dad519636d8e7980d72a5284b3de708febd300bb1fc1525f785b.bpt");
-        if(random.nextBoolean())
-        	stream = ComponentPowerPlant.class.getResourceAsStream("/assets/bcadditions/blueprints/Stilingengine-6a8295b667031a4687a626ecb7c6bef997dc7e62c19f9e0941c64b33198f87d4.bpt");
-        	FileInputStream inputStream = new FileInputStream(new File(getClass().getResource("/assets/bcadditions/blueprints/Redstoneengine-357d6c42def2dad519636d8e7980d72a5284b3de708febd300bb1fc1525f785b.bpt").toURI()));
-        int i = this.boundingBox.minX;
-        int j = this.boundingBox.minY;
-        int k = this.boundingBox.minZ;
+        URL url = ComponentPowerPlant.class.getResource("/assets/bcadditions/blueprints/Redstoneengine-357d6c42def2dad519636d8e7980d72a5284b3de708febd300bb1fc1525f785b.bpt");
+        try{
+        InputStream f =  Minecraft.getMinecraft().getResourceManager().getResource(blueprint).getInputStream();
+        
+        		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 
-		try {
-			byte [] data = new byte [(int) file.length()];
-			f.read (data);
-			f.close();
-			
-			BlueprintDeployer.instance.deployBlueprintFromFileStream(world, i, j-3, k, ForgeDirection.getOrientation(this.getMetadataWithOffset(BuildCraftBuilders.builderBlock, 0)), data);
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}*/
+        		int nRead;
+        		byte[] data = new byte[16384];
+
+        		while ((nRead = f.read(data, 0, data.length)) != -1) {
+        		  buffer.write(data, 0, nRead);
+        		}
+
+        		buffer.flush();
+
+        		data =  buffer.toByteArray();
+        		int i = this.boundingBox.minX;
+                int j = this.boundingBox.minY;
+                int k = this.boundingBox.minZ;
+        		BlueprintDeployer.instance.deployBlueprintFromFileStream(world, i, j-3, k, ForgeDirection.getOrientation(this.getMetadataWithOffset(BuildCraftBuilders.builderBlock, 0)), data);
+        } catch (Throwable e){
+        	System.out.println(e.getStackTrace());
+        }
+        
+        
         	
         spawnVillagers(world, sbb, 0, 0, 0, 2);
         return true;
