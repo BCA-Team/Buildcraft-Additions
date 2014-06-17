629be7bdf71fdf1d15ad29cf8087e140b14e965c
AEnterprise <aenterprise2@gmail.com>
2014-06-17 16:27:44 +0200

    added power variables to the config file

--- src/main/java/buildcraftAdditions/core/Configuration.java
+++ src/main/java/buildcraftAdditions/core/Configuration.java
@@ -12,8 +12,11 @@ import net.minecraftforge.common.config.Property;
  */
 public class Configuration {
     public static boolean shouldPrintChangelog;
+    public static int[] powerDifficultyModifiers;
+    public static int basePowerModifier;
 
     public static void readConfig(FMLPreInitializationEvent event){
+        powerDifficultyModifiers = new int[4];
         net.minecraftforge.common.config.Configuration config = new net.minecraftforge.common.config.Configuration(event.getSuggestedConfigurationFile());
         config.load();
 
@@ -25,6 +28,26 @@ public class Configuration {
 
         Property shouldPrintOutChangelog = config.get("Updates", "shouldPrintOutChangelog", false);
         shouldPrintChangelog = shouldPrintOutChangelog.getBoolean(false);
+
+
+
+        config.addCustomCategoryComment("Power Usage", "Modify how much energy the tools use");
+
+        Property peacefullModifier = config.get("Power Usage", "PeacefullDifficultyModifier", 1);
+        powerDifficultyModifiers[0] = peacefullModifier.getInt();
+
+        Property easyModifier = config.get("Power Usage", "EasyDifficultyModifier", 2);
+        powerDifficultyModifiers[1] = easyModifier.getInt();
+
+        Property normalModifier = config.get("Power Usage", "NormalDifficultyModifier", 3);
+        powerDifficultyModifiers[2] = normalModifier.getInt();
+
+        Property hardModifier = config.get("Power Usage", "HardDifficultyModifier", 4);
+        powerDifficultyModifiers[3] = hardModifier.getInt();
+
+        Property baseModifier = config.get("Power Usage", "BaseModifier", 10);
+        basePowerModifier = baseModifier.getInt();
+
 
         if (config.hasChanged())
             config.save();
