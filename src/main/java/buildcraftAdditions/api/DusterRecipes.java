e5f6216b0ea5be39f29563ee14b940ff49df2a58
Speiger <moritzstehr@gmx.de>
2014-11-15 15:57:38 +0100

    -Overrided Duster Recipe List. -Made it cleaner, added OreDictionary Support, and Added support to custom Recipes. At least at input. NBTSupport is not inclueded. But can be hardcoded. ^^"

--- src/main/java/buildcraftAdditions/api/DusterRecipes.java
+++ src/main/java/buildcraftAdditions/api/DusterRecipes.java
@@ -1,11 +1,14 @@
 package buildcraftAdditions.api;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-
 import net.minecraftforge.oredict.OreDictionary;
+import buildcraftAdditions.api.IStackInfo.ItemData;
 
 /**
  * Copyright (c) 2014, AEnterprise
@@ -18,7 +21,7 @@ public class DusterRecipes {
 
     private static final DusterRecipes dusting = new DusterRecipes();
 
-    private final HashMap<ItemStack, ItemStack> dustingList = new HashMap<ItemStack, ItemStack>();
+    private final HashMap<IStackInfo, ItemStack> dustingList = new HashMap<IStackInfo, ItemStack>();
 
     private DusterRecipes() {
     }
@@ -26,34 +29,61 @@ public class DusterRecipes {
     public static DusterRecipes dusting() {
         return dusting;
     }
+    
+    public void addDusterRecipe(String oreInput, ItemStack output)
+    {
+    	ArrayList<ItemStack> inputs = OreDictionary.getOres(oreInput);
+    	if(inputs != null && inputs.size() > 0)
+    	{
+    		for(ItemStack stack : inputs)
+    		{
+    			addDusterRecipe(stack, output);
+    		}
+    	}
+    }
+    /**
+     * If you make your own IStackInfoClasses then you can do that but make the comparing match with ItemData,
+     * because thats what this recipe list is comparing with. As you can see.
+     */
+    public void addDusterRecipe(IStackInfo par1, ItemStack output)
+    {
+    	dustingList.put(par1, output);
+    }
+    
+    public void addDusterRecipe(Block input, ItemStack output)
+    {
+    	addDusterRecipe(Item.getItemFromBlock(input), output);
+    }
+    
+    public void addDusterRecipe(Item input, ItemStack output)
+    {
+    	ItemData data = new ItemData(input);
+    	dustingList.put(data, output);
+    }
+    //Reason because if modders want to add recipes it makes codes more cleaner because adds new ItemStacks always is longer code.
+    //can be removed.
+    public void addDusterRecipe(Item input, int inputMeta, ItemStack output)
+    {
+    	ItemData data = new ItemData(input, inputMeta);
+    	dustingList.put(data, output);
+    }
 
-    public void addDusterRecipe(ItemStack input, ItemStack output) {
-        input.stackSize = 1;
-        dustingList.put(input.copy(), output.copy());
+    public void addDusterRecipe(ItemStack input, ItemStack output) 
+    {
+    	ItemData data = new ItemData(input);
+    	dustingList.put(data, output);
     }
 
     public ItemStack getDustingResult(ItemStack input) {
-        if (input != null) {
-            for (Entry<ItemStack, ItemStack> entry : dusting.dustingList.entrySet()) {
-                if (areItemStacksSameTypeCrafting(input, entry.getKey())) {
-                    return entry.getValue().copy();
-                }
-            }
-        }
-        return null;
+    	return dustingList.get(new ItemData(input));
     }
 
     public boolean hasDustingResult(ItemStack input) {
         return getDustingResult(input) != null;
     }
 
-    @SuppressWarnings("unchecked")
-    public HashMap<ItemStack, ItemStack> getDustingList() {
-        return (HashMap<ItemStack, ItemStack>) dustingList.clone();
-    }
-
-    private boolean areItemStacksSameTypeCrafting(ItemStack stack1, ItemStack stack2) {
-        return stack2.getItem() == stack1.getItem() && (stack2.getItemDamage() == OreDictionary.WILDCARD_VALUE || stack2.getItemDamage() == stack1.getItemDamage());
+    public HashMap<IStackInfo, ItemStack> getDustingList() {
+        return (HashMap<IStackInfo, ItemStack>) dustingList.clone();
     }
 
 }
