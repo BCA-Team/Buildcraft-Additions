53ab5ce744a9486e2cbb71cd0fca2567e0a508ce
AEnterprise <aenterprise2@gmail.com>
2014-12-08 15:45:29 +0100

    update BC api to 6.2.2

--- src/main/java/buildcraft/api/core/Position.java
+++ src/main/java/buildcraft/api/core/Position.java
@@ -8,13 +8,11 @@
  */
 package buildcraft.api.core;
 
+import io.netty.buffer.ByteBuf;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
 import net.minecraftforge.common.util.ForgeDirection;
-
-
-import io.netty.buffer.ByteBuf;
 
 public class Position implements ISerializable {
 
