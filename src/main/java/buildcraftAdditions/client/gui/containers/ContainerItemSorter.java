cf1ac25ebcd6edb7e1157a54cfde1062ade417f9
big_Xplosion <bigxplosion44@gmail.com>
2015-01-19 19:31:25 +0100

    Added slots to the container

--- src/main/java/buildcraftAdditions/client/gui/containers/ContainerItemSorter.java
+++ src/main/java/buildcraftAdditions/client/gui/containers/ContainerItemSorter.java
@@ -2,6 +2,7 @@ package buildcraftAdditions.client.gui.containers;
 
 import net.minecraft.entity.player.InventoryPlayer;
 
+import buildcraftAdditions.client.gui.SlotPhantom;
 import buildcraftAdditions.tileEntities.TileItemSorter;
 
 /**
@@ -13,7 +14,21 @@ import buildcraftAdditions.tileEntities.TileItemSorter;
  */
 public class ContainerItemSorter extends ContainerBase {
 
-	public ContainerItemSorter(InventoryPlayer PlayerInv, TileItemSorter tile) {
+	public TileItemSorter tile;
 
+	public ContainerItemSorter(InventoryPlayer playerInv, TileItemSorter tile) {
+		this.tile = tile;
+
+		for (int i = 0; i < 8; i++)
+			addSorterSlotColumn(26 + i * 18, 18, i * 6);
+
+		addPlayerInventory(playerInv, 8, 160);
+	}
+
+	public void addSorterSlotColumn(int x, int y, int id) {
+		for (int i = 0; i < 6; i++) {
+			addSlotToContainer(new SlotPhantom(tile, id, x, y + i * 18));
+			id++;
+		}
 	}
 }
