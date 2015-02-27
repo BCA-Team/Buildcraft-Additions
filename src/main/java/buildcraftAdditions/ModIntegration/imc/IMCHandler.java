ca5b60decf05914309321499f73094dd49a52108
iTitus <iTitus@users.noreply.github.com>
2015-02-27 16:09:28 +0100

    Added IMC messages for adding and removing dusts.

--- src/main/java/buildcraftAdditions/ModIntegration/imc/IMCHandler.java
+++ src/main/java/buildcraftAdditions/ModIntegration/imc/IMCHandler.java
@@ -2,6 +2,8 @@ package buildcraftAdditions.ModIntegration.imc;
 
 import java.util.List;
 
+import com.google.common.base.Strings;
+
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
@@ -10,13 +12,14 @@ import cpw.mods.fml.common.event.FMLInterModComms;
 import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fluids.FluidStack;
 
+import buildcraftAdditions.api.item.BCAItemManager;
+import buildcraftAdditions.api.item.dust.IDustType;
 import buildcraftAdditions.api.recipe.BCARecipeManager;
 import buildcraftAdditions.core.Logger;
-
-import com.google.common.base.Strings;
+import buildcraftAdditions.items.dust.DustTypes;
 
 /**
- * Copyright (c) 2014, AEnterprise
+ * Copyright (c) 2014-2015, AEnterprise
  * http://buildcraftadditions.wordpress.com/
  * Buildcraft Additions is distributed under the terms of GNU GPL v3.0
  * Please check the contents of the license located in
@@ -32,116 +35,194 @@ public class IMCHandler {
 			if (Strings.isNullOrEmpty(type))
 				continue;
 
-			if (type.equals("addDustingRecipe")) {
+			if (type.equalsIgnoreCase("addDustingRecipe")) {
 				if (!message.isNBTMessage()) {
 					logNotNBT(message);
 					continue;
 				}
 
 				NBTTagCompound tag = message.getNBTValue();
-				if (!checkRequiredNBTTags("Dusting", tag, "Input", "Output"))
+				if (!checkRequiredNBTTags(message, tag, "Input", "Output", Constants.NBT.TAG_COMPOUND))
 					continue;
 
-				if (tag.hasKey("Output", Constants.NBT.TAG_COMPOUND)) {
-					ItemStack output = ItemStack.loadItemStackFromNBT(tag.getCompoundTag("Output"));
-					if (output == null)
+				ItemStack output = ItemStack.loadItemStackFromNBT(tag.getCompoundTag("Output"));
+				if (output == null) {
+					logWrongNBT(message);
+					continue;
+				}
+				if (tag.hasKey("Input", Constants.NBT.TAG_STRING)) {
+					String oreInput = tag.getString("Input");
+					if (!Strings.isNullOrEmpty(oreInput))
+						BCARecipeManager.duster.addRecipe(oreInput, output);
+					else {
+						logWrongNBT(message);
 						continue;
-					if (tag.hasKey("Input", Constants.NBT.TAG_STRING)) {
-						String oreInput = tag.getString("Input");
-						if (!Strings.isNullOrEmpty(oreInput))
-							BCARecipeManager.duster.addRecipe(oreInput, output);
-					} else if (tag.hasKey("Input", Constants.NBT.TAG_COMPOUND)) {
-						ItemStack input = ItemStack.loadItemStackFromNBT(tag.getCompoundTag("Input"));
-						if (input != null)
-							BCARecipeManager.duster.addRecipe(input, output);
 					}
-				}
-			} else if (type.equals("addCoolingTowerRecipe")) {
-				if (!message.isNBTMessage()) {
-					logNotNBT(message);
-					continue;
-				}
-
-				NBTTagCompound tag = message.getNBTValue();
-				if (!checkRequiredNBTTags("Cooling Tower", tag, "Input", "Output", "Heat"))
-					continue;
-
-				if (tag.hasKey("Input", Constants.NBT.TAG_COMPOUND) && tag.hasKey("Output", Constants.NBT.TAG_COMPOUND) && tag.hasKey("Heat", Constants.NBT.TAG_FLOAT)) {
-					FluidStack input = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Input"));
-					if (input == null)
-						continue;
-					FluidStack output = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Output"));
-					if (output == null)
-						continue;
-					BCARecipeManager.cooling.addRecipe(input, output, tag.getFloat("Heat"));
-				}
-			} else if (type.equals("addRefineryRecipe")) {
-				if (!message.isNBTMessage()) {
-					logNotNBT(message);
-					continue;
-				}
-
-				NBTTagCompound tag = message.getNBTValue();
-				if (!checkRequiredNBTTags("Refinery", tag, "Input", "Output", "RequiredHeat"))
-					continue;
-
-				if (tag.hasKey("Input", Constants.NBT.TAG_COMPOUND) && tag.hasKey("Output", Constants.NBT.TAG_COMPOUND) && tag.hasKey("RequiredHeat", Constants.NBT.TAG_INT)) {
-					FluidStack input = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Input"));
-					if (input == null)
-						continue;
-					FluidStack output = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Output"));
-					if (output == null)
-						continue;
-					BCARecipeManager.refinery.addRecipe(input, output, tag.getInteger("RequiredHeat"));
-				}
-			} else if (type.equals("removeDustingRecipe")) {
-				if (!message.isNBTMessage()) {
-					logNotNBT(message);
-					continue;
-				}
-
-				NBTTagCompound tag = message.getNBTValue();
-				if (!checkRequiredNBTTags("Dusting", tag, "Input"))
-					continue;
-
-				if (tag.hasKey("Input", Constants.NBT.TAG_COMPOUND)) {
+				} else if (tag.hasKey("Input", Constants.NBT.TAG_COMPOUND)) {
 					ItemStack input = ItemStack.loadItemStackFromNBT(tag.getCompoundTag("Input"));
-					if (input == null)
+					if (input != null)
+						BCARecipeManager.duster.addRecipe(input, output);
+					else {
+						logWrongNBT(message);
 						continue;
-					BCARecipeManager.duster.removeRecipe(input);
+					}
+				} else {
+					logError(message, "Missing required NBT Tag 'Input' of type STRING or TAG_COMPOUND!");
+					continue;
 				}
-			} else if (type.equals("removeCoolingRecipe")) {
+			} else if (type.equalsIgnoreCase("addCoolingTowerRecipe")) {
 				if (!message.isNBTMessage()) {
 					logNotNBT(message);
 					continue;
 				}
 
 				NBTTagCompound tag = message.getNBTValue();
-				if (!checkRequiredNBTTags("Cooling Tower", tag, "Input"))
+				if (!checkRequiredNBTTags(message, tag, "Input", Constants.NBT.TAG_COMPOUND, "Output", Constants.NBT.TAG_COMPOUND, "Heat", Constants.NBT.TAG_FLOAT))
 					continue;
 
-				if (tag.hasKey("Input", Constants.NBT.TAG_COMPOUND)) {
-					FluidStack input = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Input"));
-					if (input == null)
-						continue;
-					BCARecipeManager.cooling.removeRecipe(input);
+				FluidStack input = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Input"));
+				if (input == null) {
+					logNotNBT(message);
+					continue;
 				}
-			} else if (type.equals("removeRefineryRecipe")) {
+				FluidStack output = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Output"));
+				if (output == null) {
+					logNotNBT(message);
+					continue;
+				}
+				BCARecipeManager.cooling.addRecipe(input, output, tag.getFloat("Heat"));
+			} else if (type.equalsIgnoreCase("addRefineryRecipe")) {
 				if (!message.isNBTMessage()) {
 					logNotNBT(message);
 					continue;
 				}
 
 				NBTTagCompound tag = message.getNBTValue();
-				if (!checkRequiredNBTTags("Refinery", tag, "Input"))
+				if (!checkRequiredNBTTags(message, tag, "Input", Constants.NBT.TAG_COMPOUND, "Output", Constants.NBT.TAG_COMPOUND, "RequiredHeat", Constants.NBT.TAG_INT))
 					continue;
 
-				if (tag.hasKey("Input", Constants.NBT.TAG_COMPOUND)) {
-					FluidStack input = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Input"));
-					if (input == null)
-						continue;
-					BCARecipeManager.refinery.removeRecipe(input);
+				FluidStack input = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Input"));
+				if (input == null) {
+					logNotNBT(message);
+					continue;
 				}
+				FluidStack output = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Output"));
+				if (output == null) {
+					logNotNBT(message);
+					continue;
+				}
+				BCARecipeManager.refinery.addRecipe(input, output, tag.getInteger("RequiredHeat"));
+			} else if (type.equalsIgnoreCase("removeDustingRecipe")) {
+				if (!message.isNBTMessage()) {
+					logNotNBT(message);
+					continue;
+				}
+
+				NBTTagCompound tag = message.getNBTValue();
+				if (!checkRequiredNBTTags(message, tag, "Input", Constants.NBT.TAG_COMPOUND))
+					continue;
+
+				ItemStack input = ItemStack.loadItemStackFromNBT(tag.getCompoundTag("Input"));
+				if (input == null) {
+					logNotNBT(message);
+					continue;
+				}
+				BCARecipeManager.duster.removeRecipe(input);
+			} else if (type.equalsIgnoreCase("removeCoolingRecipe")) {
+				if (!message.isNBTMessage()) {
+					logNotNBT(message);
+					continue;
+				}
+
+				NBTTagCompound tag = message.getNBTValue();
+				if (!checkRequiredNBTTags(message, tag, "Input", Constants.NBT.TAG_COMPOUND))
+					continue;
+
+				FluidStack input = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Input"));
+				if (input == null) {
+					logNotNBT(message);
+					continue;
+				}
+				BCARecipeManager.cooling.removeRecipe(input);
+			} else if (type.equalsIgnoreCase("removeRefineryRecipe")) {
+				if (!message.isNBTMessage()) {
+					logNotNBT(message);
+					continue;
+				}
+
+				NBTTagCompound tag = message.getNBTValue();
+				if (!checkRequiredNBTTags(message, tag, "Input", Constants.NBT.TAG_COMPOUND))
+					continue;
+
+				FluidStack input = FluidStack.loadFluidStackFromNBT(tag.getCompoundTag("Input"));
+				if (input == null) {
+					logNotNBT(message);
+					continue;
+				}
+				BCARecipeManager.refinery.removeRecipe(input);
+			} else if (type.equalsIgnoreCase("addMetalDust")) {
+				if (!message.isNBTMessage()) {
+					logNotNBT(message);
+					continue;
+				}
+
+				NBTTagCompound tag = message.getNBTValue();
+				if (!checkRequiredNBTTags(message, tag, "Meta", Constants.NBT.TAG_INT, "Name", Constants.NBT.TAG_STRING, "ColorMultiplier", Constants.NBT.TAG_INT))
+					continue;
+
+				BCAItemManager.dusts.addDust(tag.getInteger("Meta"), tag.getString("Name"), tag.getInteger("ColorMultiplier"), DustTypes.METAL_DUST);
+			} else if (type.equalsIgnoreCase("addGemDust")) {
+				if (!message.isNBTMessage()) {
+					logNotNBT(message);
+					continue;
+				}
+
+				NBTTagCompound tag = message.getNBTValue();
+				if (!checkRequiredNBTTags(message, tag, "Meta", Constants.NBT.TAG_INT, "Name", Constants.NBT.TAG_STRING, "ColorMultiplier", Constants.NBT.TAG_INT))
+					continue;
+
+				BCAItemManager.dusts.addDust(tag.getInteger("Meta"), tag.getString("Name"), tag.getInteger("ColorMultiplier"), DustTypes.GEM_DUST);
+			} else if (type.equalsIgnoreCase("addDust")) {
+				if (!message.isNBTMessage()) {
+					logNotNBT(message);
+					continue;
+				}
+
+				NBTTagCompound tag = message.getNBTValue();
+				if (!checkRequiredNBTTags(message, tag, "Meta", Constants.NBT.TAG_INT, "Name", Constants.NBT.TAG_STRING, "ColorMultiplier", Constants.NBT.TAG_INT, "DustType", Constants.NBT.TAG_STRING))
+					continue;
+
+				String className = tag.getString("DustType");
+				if (Strings.isNullOrEmpty(className)) {
+					logWrongNBT(message);
+					continue;
+				}
+				IDustType iDustType;
+				try {
+					iDustType = (IDustType) Class.forName(className).newInstance();
+				} catch (Throwable t) {
+					logWrongNBT(message);
+					continue;
+				}
+				BCAItemManager.dusts.addDust(tag.getInteger("Meta"), tag.getString("Name"), tag.getInteger("ColorMultiplier"), iDustType);
+			} else if (type.equalsIgnoreCase("removeDust")) {
+				if (!message.isStringMessage()) {
+					logNotString(message);
+					continue;
+				}
+
+				String string = message.getStringValue();
+				if (Strings.isNullOrEmpty(string)) {
+					logError(message, "The string is null or empty!");
+					continue;
+				}
+				try {
+					BCAItemManager.dusts.removeDust(Integer.valueOf(string));
+				} catch (NumberFormatException e) {
+					BCAItemManager.dusts.removeDust(string);
+				}
+			} else {
+				logError(message, "Message type not supported!");
 			}
 		}
 	}
@@ -159,18 +240,51 @@ public class IMCHandler {
 	}
 
 	private static void logInvalidMessage(FMLInterModComms.IMCMessage message, String type) {
-		Logger.error(String.format("Received and invalid IMC message : '%s' from '%s'. This message is not of the '%s' type!", message.key, message.getSender(), type));
+		logError(message, "This message is not of the type '%s'!", type);
 	}
 
-	private static boolean checkRequiredNBTTags(String key, NBTTagCompound tag, String... requiredArgs) {
+	private static void logWrongNBT(FMLInterModComms.IMCMessage message) {
+		logError(message, "This message contains (a/an) wrong/corrupt/incomplete NBT tag(s)!");
+	}
+
+	private static void logError(FMLInterModComms.IMCMessage message, String string) {
+		Logger.error(String.format("Received an invalid IMC message: '%s' from '%s'. " + string, message.key, message.getSender()));
+	}
+
+	private static void logError(FMLInterModComms.IMCMessage message, String string, Object... args) {
+		Object[] formatArgs = new Object[(args != null ? args.length : 0) + 2];
+		formatArgs[0] = message.key;
+		formatArgs[1] = message.getSender();
+		for (int i = 2; i < formatArgs.length; i++)
+			formatArgs[i] = args[i - 2];
+		logError(message, String.format(string, formatArgs));
+	}
+
+	private static boolean checkRequiredNBTTags(FMLInterModComms.IMCMessage message, NBTTagCompound tag, Object... requiredArgs) {
 		boolean fine = true;
-		for (String arg : requiredArgs) {
-			if (!tag.hasKey(arg)) {
-				Logger.error(String.format("%s IMC: Missing required NBT Tag '%s'", key, arg));
-				fine = false;
+		for (int i = 0; i < requiredArgs.length; i++) {
+			Object o = requiredArgs[i];
+			if (o == null || !(o instanceof String))
+				continue;
+			String tagKey = (String) o;
+			Object tagType = null;
+			if (i < requiredArgs.length - 1)
+				tagType = requiredArgs[i + 1];
+			if (tagType != null && tagType instanceof Integer) {
+				int tagTypeCode = (Integer) tagType;
+				if (!tag.hasKey(tagKey, tagTypeCode)) {
+					logError(message, "Missing required NBT Tag '%s' of type %d!", tagKey, tagTypeCode);
+					fine = false;
+				}
+			} else {
+				if (!tag.hasKey(tagKey)) {
+					logError(message, "Missing required NBT Tag '%s'!", tagKey);
+					fine = false;
+				}
 			}
 		}
 
 		return fine;
 	}
+
 }
--- src/main/java/buildcraftAdditions/items/dust/DustManager.java
+++ src/main/java/buildcraftAdditions/items/dust/DustManager.java
@@ -28,46 +28,78 @@ public class DustManager implements IDustManager {
 
 	@Override
 	public void addDust(int meta, String name, int colorMultiplier, IDustType dustType) {
-		if (meta < 0 || meta >= dusts.length || StringUtils.isNullOrEmpty(name) || Character.isLowerCase(name.charAt(0)) || dustType == null || !ConfigurationHandler.shouldRegisterDusts) {
-			Logger.error("Tried to register an invalid dust! Skipping.");
+		if (!ConfigurationHandler.shouldRegisterDusts) {
+			Logger.debug("Dust registering is disabled via config.");
+			Logger.debug("Was trying to add: Meta: " + meta + ", Name: " + name + ", Color multiplier: " + colorMultiplier + ", Dust type: " + (dustType != null ? dustType.getName() : "null"));
+			return;
+		}
+		if (meta < 0 || meta >= dusts.length) {
+			Logger.error("Tried to register an invalid dust! The meta '" + meta + "' is out of bounds! Skipping.");
+			Logger.error("Was trying to add: Meta: " + meta + ", Name: " + name + ", Color multiplier: " + colorMultiplier + ", Dust type: " + (dustType != null ? dustType.getName() : "null"));
+			return;
+		}
+		if (StringUtils.isNullOrEmpty(name) || Character.isLowerCase(name.charAt(0))) {
+			Logger.error("Tried to register an invalid dust! The name '" + name + "' is not valid! Skipping.");
+			Logger.error("Was trying to add: Meta: " + meta + ", Name: " + name + ", Color multiplier: " + colorMultiplier + ", Dust type: " + (dustType != null ? dustType.getName() : "null"));
+			return;
+		}
+		if (dustType == null) {
+			Logger.error("Tried to register an invalid dust! The dust type must not be null! Skipping.");
+			Logger.error("Was trying to add: Meta: " + meta + ", Name: " + name + ", Color multiplier: " + colorMultiplier + ", Dust type: " + (dustType != null ? dustType.getName() : "null"));
 			return;
 		}
 		IDust dust = getDust(meta);
 		if (dust != null) {
 			Logger.error("A dust with the meta '" + meta + "' is already registered! Skipping.");
-			Logger.error("Was trying to add: Meta: " + meta + " Name: " + name + " Color multiplier: " + colorMultiplier + " Dust type: " + dustType.getName());
-			Logger.error("Found: Meta: " + meta + " Name: " + dust.getName() + " Color multiplier: " + dust.getColorMultiplier() + " Dust type: " + dust.getDustType().getName());
+			Logger.error("Was trying to add: Meta: " + meta + ", Name: " + name + ", Color multiplier: " + colorMultiplier + ", Dust type: " + (dustType != null ? dustType.getName() : "null"));
+			Logger.error("Found: Meta: " + dust.getMeta() + ", Name: " + dust.getName() + ", Color multiplier: " + dust.getColorMultiplier() + ", Dust type: " + (dust.getDustType() != null ? dust.getDustType().getName() : "null"));
 			return;
 		}
 		dust = getDust(name);
 		if (dust != null) {
 			Logger.error("A dust with the name '" + name + "' is already registered! Skipping.");
-			Logger.error("Was trying to add: Meta: " + meta + " Name: " + name + " Color multiplier: " + colorMultiplier + " Dust type: " + dustType.getName());
-			Logger.error("Found: Meta: " + meta + " Name: " + dust.getName() + " Color multiplier: " + dust.getColorMultiplier() + " Dust type: " + dust.getDustType().getName());
+			Logger.error("Was trying to add: Meta: " + meta + ", Name: " + name + ", Color multiplier: " + colorMultiplier + ", Dust type: " + (dustType != null ? dustType.getName() : "null"));
+			Logger.error("Found: Meta: " + dust.getMeta() + ", Name: " + dust.getName() + ", Color multiplier: " + dust.getColorMultiplier() + ", Dust type: " + (dust.getDustType() != null ? dust.getDustType().getName() : "null"));
 			return;
 		}
 		dust = new Dust(meta, name, colorMultiplier, dustType);
 		if (dustType.isValid(meta, name, dust.getDustStack())) {
 			dusts[meta] = dust;
-			//add converters
 			Item converter = new ItemConverter(dust);
 			GameRegistry.registerItem(converter, "converter" + name.toLowerCase());
+			Logger.info("Successfully added a dust: Meta: " + meta + ", Name: " + name + ", Color multiplier: " + colorMultiplier + ", Dust type: " + (dustType != null ? dustType.getName() : "null"));
+		} else {
+			Logger.debug("The dust with the name '" + name + "' will not be registered because it is invalid in this environment! Skipping.");
+			Logger.debug("Was trying to add: Meta: " + meta + ", Name: " + name + ", Color multiplier: " + colorMultiplier + ", Dust type: " + (dustType != null ? dustType.getName() : "null"));
 		}
 	}
 
 	@Override
 	public void removeDust(int meta) {
+		IDust dust = getDust(meta);
+		if (dust == null) {
+			Logger.error("Tried to remove an invalid dust! A dust with the meta '" + meta + "' could not be found! Skipping.");
+			return;
+		}
 		if (meta >= 0 && meta < dusts.length) {
 			dusts[meta] = null;
+			Logger.info("Successfully removed dust: Meta: " + dust.getMeta() + ", Name: " + dust.getName() + ", Color multiplier: " + dust.getColorMultiplier() + ", Dust type: " + (dust.getDustType() != null ? dust.getDustType().getName() : "null"));
+			return;
 		}
 	}
 
 	@Override
 	public void removeDust(String name) {
+		IDust dust = getDust(name);
+		if (dust == null) {
+			Logger.error("Tried to remove an invalid dust! A dust with the name '" + name + "' could not be found! Skipping.");
+			return;
+		}
 		if (!StringUtils.isNullOrEmpty(name)) {
 			for (int i = 0; i < dusts.length; i++) {
 				if (dusts[i] != null && dusts[i].getName().equalsIgnoreCase(name)) {
 					dusts[i] = null;
+					Logger.info("Successfully removed dust: Meta: " + dust.getMeta() + ", Name: " + dust.getName() + ", Color multiplier: " + dust.getColorMultiplier() + ", Dust type: " + (dust.getDustType() != null ? dust.getDustType().getName() : "null"));
 					return;
 				}
 			}
