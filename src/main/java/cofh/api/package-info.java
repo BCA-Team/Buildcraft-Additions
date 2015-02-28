dae6746aa4b8e74bde997a76812d262e38ba87ad
AEnterprise <aenterprise2@gmail.com>
2015-02-28 16:18:41 +0100

    BCA 2.0.2

---
+++ src/main/java/cofh/api/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * (C) 2015 Team CoFH / CoFH / Cult of the Full Hub
+ * http://www.teamcofh.com
+ */
+@API(apiVersion = CoFHAPIProps.VERSION, owner = "CoFHLib", provides = "CoFHAPI")
+package cofh.api;
+
+import cpw.mods.fml.common.API;
+
